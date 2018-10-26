50c083f80a9fda3219a1ad2a22f178289f33f587
ChrisHayes1 <hayesbirchle@wisc.edu>
2018-10-26 09:22:37 -0500

    Renamed some items no longer being used as old_ and added some comments

--- old_server_MySQL.js
+++ old_server_MySQL.js
@@ -1,14 +1,3 @@
-/******************************************************
- * All Paws On Deck
- * CS 506
- * Server.js
- * Provides key server functionality using Express.  Brings in basic tools
- * for tracking.
- * 
- * Initial structure for key server components taken from 
- * https://scotch.io/tutorials/easy-node-authentication-setup-and-local#undefined
- * 
- *****************************************************/
 // server.js
 
 // set up ======================================================================
@@ -16,6 +5,7 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 80;
+//var mysql = require('mysql');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -26,10 +16,21 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
 //var configDB = require('./config/database.js');
-
+//var config = require('./config/mysql.js');
+//var knex_con = require('./config/knex_config.js');
+//var Knex = require('knex')({ knex_con }); //connect through knex
+//var knex = require('knex')({
+//	client:'mysql',
+//	host: '127.0.0.1',
+//	user: 'furever',
+//	password:'fuever1234',
+//	database:'APOD'
+//});
+//const{ Model }= require('objection');
 // configuration ===============================================================
-mongoose.connect('mongodb://myUserAdmin:abc123@127.0.0.1:27017'); // connect to our database
-
+//mysql.createConnection(config.pass); // connect to our database
+//Model.knex(knex);
+mongoose.connect('mongodb://myUserAdmin:abc123@localhost:27017');
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
@@ -40,7 +41,7 @@ app.use(bodyParser()); // get information from html forms
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'AllPawsBestTeamEver' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -51,3 +52,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 // launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
+
