c094309179f6df573a9b58b222af37f3c5cb2565
root <root@apod.us-central1-c.c.allpawsondeck-218923.internal>
2018-10-26 01:17:13 +0000

    Some additional base files loaded

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,12 @@ module.exports = function(app, passport) {
 
     // process the login form
     // app.post('/login', do all our passport stuff here);
-
+// process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
     // =====================================
     // SIGNUP ==============================
     // =====================================
