438ec9a3e221802217269bba62907adee0ce3e87
Jarrett Benson <jbenson6@wisc.edu>
2018-10-29 18:31:22 -0500

    Added VolunteerPostions class

---
+++ app/models/VolunteerPositions.js
@@ -0,0 +1,11 @@
+class VolunteerPositions(postionID, postionName, roleDescription){
+
+	this.postionID = postionID;
+	this.postionName = postionName;
+	this.roleDescription = roleDescription;
+
+	this.trainings = function() {
+		
+	}
+
+}
