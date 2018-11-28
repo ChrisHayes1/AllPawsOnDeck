6a96a2564c9ce3b3212ac60425e0604870675b51
Logan Miller <logan@Logans-MacBook-Pro.local>
2018-11-28 00:45:30 -0600

    add events to calendar, basic framework for events

---
+++ public/events.js
@@ -0,0 +1,32 @@
+var events = function() {
+    return [{
+    id: 1,
+    title: "Well, you said you wanted to be around when I made a mistake.",
+    start: "2018-11-03T14:30:00.000Z",
+    end: null
+    },
+    {
+    id: 2,
+    title: "You'll find I'm full of surprises!",
+    start: "2018-11-10T16:30:00.000Z",
+    end: null
+    },
+    {
+    id: 3,
+    title: "Run tests",
+    start: "2018-11-12T12:30:00.000Z",
+    end: null
+    },
+    {
+    id: 4,
+    title: "Theon Greyjoy",
+    start: "2018-11-13T13:30:00.000Z",
+    end: null
+    },
+    {
+    id: 5,
+    title: "Lilycove City",
+    start: "2018-11-04T14:30:00.000Z",
+    end: null
+    }]
+}
